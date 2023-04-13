8655f03fc7944b2f8c56a5187e529ae02e54dbdf
EC2 Default User <ec2-user@ip-172-31-61-198.ec2.internal>
2023-04-13 12:51:11 +0000

    add message_dispatcher.py

--- message_dispatcher.py
+++ message_dispatcher.py
@@ -4,12 +4,12 @@ import requests
 
 def send_msg(message: str) -> None:
 
-    apiToken = 'TBD'
-    chatID = 'TBD'
-    apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+    apiToken = "TBD"  # set once a telegram bot is created
+    chatID = "TBD"   # set once a telegram bot is created
+    apiURL = f"https://api.telegram.org/bot{apiToken}/sendMessage"
 
     try:
-        response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
+        response = requests.post(apiURL, json={"chat_id": chatID, "text": message})
         print(response.text)
     except Exception as e:
         print(e)
