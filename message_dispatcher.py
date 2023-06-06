445e3bdaa6d529aa8b1ad9e9f9807b6c45f57a5a
0xlmak <ludovic.makong@gmail.com>
2023-06-06 14:48:23 +0000

    add revisions on message_dispatcher.py

--- message_dispatcher.py
+++ message_dispatcher.py
@@ -5,7 +5,7 @@ import telebot
 apiToken = "TBD"  # set once a telegram bot is created
 bot = telebot.TeleBot(apiToken)
 
-@bot.message_handler(commands = ["start"])
+@bot.message_handler(commands=["start"])
 def send_msg(message: str) -> None:
     bot.send_message(message.chat.id, message, parse_mode="Markdown")
 
