cca531612010537e4a232090bd5a376e5f04e8f9
0xlmak <ludovic.makong@gmail.com>
2023-06-12 14:33:31 +0000

    add revisions to data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from dataclasses import dataclass
 from diskcache import Cache
 import feedparser
@@ -7,7 +8,7 @@ import feedparser
 class RSSDataSource:
     rss_url: str
 
-    def get_data(self):
+    def get_data(self) -> Dict[str, str]:
         feed = feedparser.parse(self.rss_url)
         # dictionary for articles details
         articles_details = {}
@@ -16,7 +17,7 @@ class RSSDataSource:
         for entry in feed.entries:
             tmp = dict()
             i += 1
-            tmp["id"] = i  
+            tmp["id"] = str(i)
             tmp["title"] = entry.title
             tmp["link"] = entry.link
             tmp["author"] = entry.author
@@ -25,12 +26,14 @@ class RSSDataSource:
             tmp["authors"] = [author.name for author in entry.authors]
             tmp["summary"] = entry.summary
             article_list.append(tmp)
+
         
         articles_details["articles"] = article_list
-        
+
+      
         return articles_details
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import json
   
     feed_url = "https://www.jeuneafrique.com/rubriques/politique/feed/"
@@ -38,8 +41,7 @@ if __name__ == '__main__':
     data = ja_feed.get_data() 
     
     if data:
-    # printing as a json string with indentation level = 2
+        # printing as a json string with indentation level = 2
         print(json.dumps(data, indent=2, ensure_ascii=False)) 
     else:
         print("None")
-
